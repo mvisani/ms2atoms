9bdfd1f32bc52c92de6162454dc2d07ca9e3fb3b
Marco Visani <92217074+mvisani@users.noreply.github.com>
2026-04-30 17:00:15 +0200

    added batch processing

--- src/data.rs
+++ src/data.rs
@@ -1,14 +1,43 @@
 use burn::{data::dataloader::batcher::Batcher, prelude::*};
 
 #[derive(Clone, Default)]
-pub struct Batcher {}
+pub struct SpectraBatcher {}
 
 #[derive(Clone, Debug)]
-pub struct Batch<B: Backend> {
+pub struct SpectraBatch<B: Backend> {
     pub spectra: Tensor<B, 2>,
-    pub targets: Tensor<B, 2, Int>,
+    pub targets: Tensor<B, 2, Bool>,
 }
 
-impl<B: Backend> Batcher<B, I, Batch<B>> for Batcher {
-    fn batch(&self, items: Vec<I>, device: &<B as Backend>::Device) -> O {}
+pub const BIN_SIZE: usize = 4096;
+pub const NUMBER_OF_ATOMS: usize = 90;
+
+#[derive(Clone)]
+struct ProcessedSpectrum {
+    spectrum: [f64; BIN_SIZE],
+    atom_present: [bool; NUMBER_OF_ATOMS],
+}
+
+impl<B: Backend> Batcher<B, ProcessedSpectrum, SpectraBatch<B>> for SpectraBatcher {
+    fn batch(
+        &self,
+        items: Vec<ProcessedSpectrum>,
+        device: &<B as Backend>::Device,
+    ) -> SpectraBatch<B> {
+        let spectra = items
+            .iter()
+            .map(|item| TensorData::from(item.spectrum).convert::<B::FloatElem>())
+            .map(|data| Tensor::<B, 2>::from_data(data, device))
+            .collect();
+
+        let targets = items
+            .iter()
+            .map(|item| Tensor::<B, 2, Bool>::from_data(item.atom_present, device))
+            .collect();
+
+        let spectra = Tensor::cat(spectra, 0);
+        let targets = Tensor::cat(targets, 0);
+
+        SpectraBatch { spectra, targets }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@
 mod data;
 mod model;
 mod training;
-use crate::model::ModelConfig;
+use crate::{data::BIN_SIZE, model::ModelConfig};
 use burn::backend::Metal;
 
 fn main() {
     type MyBackend = Metal;
 
     let device = Default::default();
-    let model = ModelConfig::new(10, 2048).init::<MyBackend>(&device);
+    let model = ModelConfig::new(10, BIN_SIZE).init::<MyBackend>(&device);
 
     println!("{model}");
 }
